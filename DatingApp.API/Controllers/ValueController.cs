0b1aeba49d64742fb265bcd1dd3b159ff9d4ef53
iman <i.amiri@virtux.nl>
2020-05-05 14:26:40 +0430

    intial commit

---
+++ DatingApp.API/Controllers/ValueController.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{ 
+    [ApiController]
+   [Route("api/[Controller]")]
+    public class ValueController:ControllerBase
+    {
+    private readonly DataContext _context;
+    public ValueController(DataContext context)
+    {
+        _context=context;
+    }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getValue(int id)
+        {
+           var redult=await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+           return Ok(redult);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getValue()
+        {
+           var redult=await _context.Values.ToListAsync();
+           return Ok(redult);
+        }
+
+    }
+}
